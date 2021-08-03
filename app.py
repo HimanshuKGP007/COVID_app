25bf6f8128d4b7dac6baf04dd83424bf6741b99e
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2021-08-03 10:25:26 +0530

    Addded Sample App references + changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from synthesizer.inference import Synthesizer
 from encoder import inference as encoder
-from vocoder import inference as vocoder
+#from vocoder import inference as vocoder
 from pathlib import Path
 import numpy as np
 import soundfile as sf
@@ -9,22 +9,28 @@ import os
 import librosa
 import glob
 from helper import draw_embed, create_spectrogram, read_audio, record, save_record
+import pickle
+
+#from setup_logging import setup_logging
+# setup_logging()
+# logger = logging.getLogger('app')
 
 "# Streamlit showcase"
-
 model_load_state = st.text("Loading pretrained models...")
 
-seed = 42
-low_mem = False
-num_generated = 0
-enc_model_fpath = Path("encoder/saved_models/pretrained.pt")
-syn_model_dir = Path("synthesizer/saved_models/logs-pretrained/")
-voc_model_fpath = Path("vocoder/saved_models/pretrained/pretrained.pt")
-encoder.load_model(enc_model_fpath)
-synthesizer = Synthesizer(
-    syn_model_dir.joinpath("taco_pretrained"), low_mem=low_mem, seed=seed
-)
-vocoder.load_model(voc_model_fpath)
+# seed = 42
+# low_mem = False
+# num_generated = 0
+# enc_model_fpath = Path("encoder/saved_models/pretrained.pt")
+# syn_model_dir = Path("synthesizer/saved_models/logs-pretrained/")
+# voc_model_fpath = Path("vocoder/saved_models/pretrained/pretrained.pt")
+# encoder.load_model(enc_model_fpath)
+# synthesizer = Synthesizer(
+#     syn_model_dir.joinpath("taco_pretrained"), low_mem=low_mem, seed=seed
+# )
+# vocoder.load_model(voc_model_fpath)
+
+loaded_model = pickle.load(open('model\finalized_model.sav', 'rb'))
 
 model_load_state.text("Loaded pretrained models!")
 
